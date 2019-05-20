148af1b85bbbb2205cf90f9a5c2531378d5764f7
BL-CoderOvertime <brandonlively@live.com>
2019-05-20 11:27:42 -0400

    Fleshed out MenuItem Model

--- app/src/main/java/com/example/projectweekunit2/MenuItem.java
+++ app/src/main/java/com/example/projectweekunit2/MenuItem.java
@@ -1,4 +1,33 @@
 package com.example.projectweekunit2;
 
 public class MenuItem {
+	private final int id, price, resturantId;
+	private final String name;
+	
+	public MenuItem(int id, int resturantId, String name) {
+		this(id,resturantId,name,0);
+	}
+	
+	public MenuItem(int id, int resturantId, String name, int price) {
+		this.id = id;
+		this.resturantId = resturantId;
+		this.name = name;
+		this.price = price;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public int getResturantId() {
+		return resturantId;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
