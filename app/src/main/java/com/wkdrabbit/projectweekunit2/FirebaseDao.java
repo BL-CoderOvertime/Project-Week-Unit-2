fb6dee6aaa86502d05de6a3e898d720c6706a34a
BL-CoderOvertime <brandonlively@live.com>
2019-05-21 17:02:35 -0400

    Finally got Firebase auth working (google auth hates me)

--- app/src/main/java/com/wkdrabbit/projectweekunit2/FirebaseDao.java
+++ app/src/main/java/com/wkdrabbit/projectweekunit2/FirebaseDao.java
@@ -1,4 +1,13 @@
 package com.wkdrabbit.projectweekunit2;
 
 public class FirebaseDao {
+	private static String UserUID = "";
+	private static final String TAG = "FirebaseDao";
+	private static final String BASE_URL = "https://foodmato.firebaseio.com/";
+	
+	
+	
+	public static void setUserUid(String userUID) {
+		UserUID = userUID;
+	}
 }
